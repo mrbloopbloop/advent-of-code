ea05b4d783907bedafa406e26d018cbefb9eb947
Nathan Stevens <mr.bloopbloop@gmail.com>
2025-12-01 00:29:24 -0600

    Advent of Code - Day 1: Complete!!!

--- 2025/day-01/README.md
+++ 2025/day-01/README.md
@@ -1,3 +1,42 @@
 # Advent of Code, 2025 - Day 1
 
+To view the complete puzzle, go [here](https://adventofcode.com/2025/day/1)
 
+## Puzzle 1
+
+The first puzzle has us figuring out a password. There is a dial numbered zero to ninetynine and instructions on how to spin the dial. The dial starts at the fifty position. Our password is defined as the number of times the dial is at the 0 position after completing a step in the instructions.
+
+### Thoughts
+
+Some things that make this puzzle easier:
+- Using a dial is similar to fineite fields in math, so using the modulo operator could be useful
+- The instructions are in a file that is well formatted. Each instruction sits on its own line. This will be easier to parse.
+
+#### Finite Fields
+
+In math there is a useful concept called finite fields. We already use them intuitivly. A good example of a finite field is a clock. If we only look at the minute hand, it is always going to be somewhere between zero minutes and fiftynine minutes. One more minute after fifty nine and we wrap around to zero. This is very much like our dial in the puzzle. We will use that to our advantage.
+
+#### Well Behaved Input
+
+In this puzzle our input is very well behaved. There are no errors or corruptions. We can lean on that. In the real world we would need to validate all the data before acting on it. It would be smart to write the code in such a way to allow for this validation to be added in the future.
+
+### Solution
+
+The code I wrote uses a `processInstruction` function. This function takes an instruction string and the current possition as an integer and then returns the new position as an integer. In the `main` function I set the initial position and a password variable. I then opened the puzzle input and iterated over each line and used the `processInstruction` function on each line. Before ending each itteration, I would check if the possition was zero. If so, I incremented the password variable. Once the for loop was complete, I print the password.
+
+## Puzzle 2
+
+Puzzle 2 reveals there is a new protocol to use! 
+Our new protocol will be almost identical to the previous one save for how we count our zeros. This new protocol includes any time our dial hits a zero *during* an instruction as well as after.
+
+### Thoughts
+
+So with this we have to modify our `processInstruction` function. I suppose there could be a new version of the function that implements the zero check and returns the number of zeros instead of the new position.
+
+However, that wouldn't work because we lose track of the current possition. Perhaps use a dictionary or dataclass? That would work nicely. I will try using a dictionary.
+
+### Solution
+
+I modified the previous `processInstruction` function. Instead of returning the position as an integer, it returned a dictionary with the position and number of times the dial showed a zero. With this I iterated over the file again this time keeping track of position and number of zeros.
+
+Apart from typos, this worked with no issues!
--- 2025/day-01/solution.py
+++ 2025/day-01/solution.py
@@ -5,16 +5,125 @@
 
 # --- Imports ---
 from rich import print
+from pathlib import Path
 
 
 
 # --- Functions ---
+def processInstruction(
+        instruction: str, 
+        currentPosition: int) -> int:
+    '''
+    precessInstruction takes an instruction string allong with the
+    current position as an integer. It then returns the new 
+    position as an integer.
+    '''
+
+    # Using a high value integer for a finite field
+    value: int = 10_000 + currentPosition
+
+    # Process instruction. Used if blocks given the two instructions.
+    if instruction[0] == 'R':
+        value += int(instruction[1:-1])
+    elif instruction[0] == 'L':
+        value -= int(instruction[1:-1])
+
+    # Using modulo to enforce the finite field
+    position: int = value % 100
+
+    return position
+
+def processMethod0x434C49434B(
+        instruction: str,
+        currentPosition: int) -> dict:
+    '''
+    processMethod0x434C49434B takes an instruction string along with
+    the current position as an integer. It then returns a dictionary
+    containing the new position as an integer and the number of times
+    the dial showed a zero value.
+    '''
+
+    # Using a high value integer for a finite field
+    value: int = 10_000 + currentPosition
+
+    # Keep track of zeros
+    zeros: int = 0
+    
+    # Keep track of rotations
+    rotations: int = int(instruction[1:-1])
+
+    # Process instruction. Used if blocks given the two instructions.
+    if instruction[0] == 'R':
+        for _ in range(rotations):
+            value += 1
+
+            # Check for zero value
+            if value % 100 == 0:
+                zeros +=1
+
+    elif instruction[0] == 'L':
+        for _ in range(rotations):
+            value -= 1
+
+            # Check for zero value
+            if value % 100 == 0:
+                zeros +=1
+
+    # Using modulo to enforce the finite field
+    position: int = value % 100
+
+    return {
+            "position":position,
+            "zeros":zeros
+            }
 
 
 
 # --- Main ---
 def main():
-    pass
+
+    # --- Puzzle 1 ---
+
+    # Set starting position
+    position: int = 50
+    password: int = 0
+
+    # Read instructions from file
+    filepath: Path = Path('./puzzle1.input')
+    with filepath.open() as file:
+        
+        # Process instructions one by one
+        for line in file:
+            position = processInstruction(line, position)
+
+            # Check if position is zero
+            if position == 0:
+                password += 1
+
+    # Report the password
+    print("--- Puzzle #1 ---")
+    print(f'The password is "{password}"')
+
+    # --- Puzzle 2 ---
+
+    # Set starting position
+    position: int = 50
+    password: int = 0
+
+    # Read instructions from file
+    with filepath.open() as file:
+        
+        # Process instructions one by one
+        for line in file:
+            data = processMethod0x434C49434B(line, position)
+            position = data["position"]
+            password += data["zeros"]
+
+
+    # Report the password
+    print("--- Puzzle #2 ---")
+    print(f'The password is "{password}"')
+    
 
 
 if __name__ == "__main__":
