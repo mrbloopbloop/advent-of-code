f5b7329eb0fb3cead8ce62c242ba85a559a815db
Nathan Stevens <mr.bloopbloop@gmail.com>
2025-11-30 22:52:52 -0600

    Boilerplate

---
+++ 2025/day-01/solution.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3.14
+
+# --- Advent of Code ---
+# ------- Day 01 -------
+
+# --- Imports ---
+from rich import print
+
+
+
+# --- Functions ---
+
+
+
+# --- Main ---
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Exitting script now!")
+        exit()
